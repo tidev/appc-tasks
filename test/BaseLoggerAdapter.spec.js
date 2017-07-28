51320942aef6ba4c9b9ce9acc26ee7a6fabf3c59
Jan Vennemann <jan.vennemann@gmx.net>
2017-07-28 21:28:23 +0200

    Implement correct tests for BaseLoggerAdapter

--- test/BaseLoggerAdapter.spec.js
+++ test/BaseLoggerAdapter.spec.js
@@ -1,12 +1,21 @@
-import { ConsoleLoggerAdapter } from '../lib/logger';
+import BaseLoggerAdapter from '../lib/logger/BaseLoggerAdapter';
 import { expect } from 'chai';
 
-describe('ConsoleLoggerAdapter', () => {
+describe('BaseLoggerAdapter', () => {
 	describe('constructor', () => {
 		it('should set task name', () => {
 			let taskName = 'constructorTest';
-			let logger = new ConsoleLoggerAdapter(taskName);
+			let logger = new BaseLoggerAdapter(taskName);
 			expect(logger._taskName).to.be.equal(taskName);
+		});
+	});
+
+	describe('prefixMessage', () => {
+		it('should prefix message with task name', () => {
+			let taskName = 'prefixTest';
+			let message = 'Prefix this!';
+			let logger = new BaseLoggerAdapter(taskName);
+			expect(logger.prefixMessage(message)).to.be.equal(`${taskName}: ${message}`);
 		});
 	});
 });
